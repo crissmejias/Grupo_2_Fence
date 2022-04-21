4c24d2af20849085348e4ed0fc35f6cbe57466ab
Cristhian Mejias Serrano <cristhianmejias495@gmail.com>
2022-04-20 21:06:19 -0500

    Agregando pantallas de listado de productos y creación de productos

--- Sprints/Sprint-3/Code/controllers/mainControllers.js
+++ Sprints/Sprint-3/Code/controllers/mainControllers.js
@@ -1,29 +1,27 @@
 // const path=require("path");
 // const { homedir } = require("os");
-const mainControllers={
-    index:(req,res) =>{
-        res.render("index");
-      },
-    productdetail:(req,res)=>{
-        res.render("productDetail");
-      },
-      quoter:(req, res) => {
-        res.render("quoter");
-      },
-      register:(req, res) => {
-        res.render("register") ;
-      },
-      login:(req, res) => {
-        res.render("login"); 
-      },
+const mainControllers = {
+  index: (req, res) => {
+    res.render("index");
+  },
+  productdetail: (req, res) => {
+    res.render("productDetail");
+  },
+  quoter: (req, res) => {
+    res.render("quoter");
+  },
+  register: (req, res) => {
+    res.render("register");
+  },
+  login: (req, res) => {
+    res.render("login");
+  },
+  products: (req, res) => {
+    res.render("productList");
+  },
+  newProduct: (req, res) => {
+    res.render("newProduct");
+  },
+};
 
-
-}
-
-
-module.exports=mainControllers;
-
-
- 
-  
- 
+module.exports = mainControllers;
--- Sprints/Sprint-3/Code/routes/main.js
+++ Sprints/Sprint-3/Code/routes/main.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const mainControllers=require("../controllers/mainControllers");
-router.get('/',mainControllers.index );
-router.get('/login', mainControllers.login);
-router.get('/productDetail', mainControllers.productdetail);
-router.get('/quoter', mainControllers.quoter);
-router.get('/register', mainControllers.register);
-
-
+const mainControllers = require("../controllers/mainControllers");
+router.get("/", mainControllers.index);
+router.get("/login", mainControllers.login);
+router.get("/productDetail", mainControllers.productdetail);
+router.get("/quoter", mainControllers.quoter);
+router.get("/register", mainControllers.register);
+router.get("/productList", mainControllers.products);
+router.get("/newProduct", mainControllers.newProduct);
 
 module.exports = router;
