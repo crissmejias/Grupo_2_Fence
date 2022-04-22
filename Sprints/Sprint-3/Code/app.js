c91cc2e09da8980cab964565facd1e69302ec681
Cristhian Mejias Serrano <cristhianmejias495@gmail.com>
2022-04-21 20:11:11 -0500

    Agregando carpetas de producto/usuario y cambios en las rutas

--- Sprints/Sprint-3/Code/app.js
+++ Sprints/Sprint-3/Code/app.js
@@ -6,10 +6,14 @@ const router = require("./routes/main");
 const app = express();
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
+app.set("views", [
+  path.join(__dirname, "views/products"),
+  path.join(__dirname, "views/users"),
+  path.join(__dirname, "views/"),
+]);
 app.listen(process.env.PORT || 3000, () =>
   console.log("Levantando un servidor con Express")
 );
-const rutaMain = require('./routes/main');
-app.use("/",rutaMain);
-
+const rutaMain = require("./routes/main");
+app.use("/", rutaMain);
