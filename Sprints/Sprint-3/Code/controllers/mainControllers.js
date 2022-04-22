1a9ed62a2b2d9a02d64f904fdc5aa0fb5f773566
Cristhian Mejias Serrano <cristhianmejias495@gmail.com>
2022-04-21 21:29:47 -0500

    Usando EJS para listado de productos

--- Sprints/Sprint-3/Code/controllers/mainControllers.js
+++ Sprints/Sprint-3/Code/controllers/mainControllers.js
@@ -1,5 +1,26 @@
 // const path=require("path");
 // const { homedir } = require("os");
+const products = [
+  {
+    id: 01,
+    tipo: "Vallas de seguridad vial",
+    material: "madera",
+    nombre: "Valla De Madera Perimetral Verde 1,20 X 1.50 Seguridad Vial",
+    precio: 1764,
+    descripcion: "xxx",
+    imagen: "images/valla-01.png",
+  },
+  {
+    id: 02,
+    tipo: "Vallas de seguridad vial",
+    material: "plastico",
+    nombre: "Valla Vial 1900x1100 Mm Polipropileno P/seguridad Vialplast",
+    precio: 33619,
+    descripcion: "yyy",
+    imagen: "images/valla-02.png",
+  },
+];
+
 const mainControllers = {
   index: (req, res) => {
     res.render("index");
@@ -17,7 +38,7 @@ const mainControllers = {
     res.render("login");
   },
   products: (req, res) => {
-    res.render("productList");
+    res.render("productList", { productos: products });
   },
   newProduct: (req, res) => {
     res.render("newProduct");
