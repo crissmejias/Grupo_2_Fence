6282c0285c0c23aa74def3763c577ca9045b8fb4
Cristhian Mejias Serrano <cristhianmejias495@gmail.com>
2022-05-03 19:28:05 -0500

    Creando archivos de producto

--- Sprints/Sprint-4/Code/app.js
+++ Sprints/Sprint-4/Code/app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const { homedir } = require("os");
 const { dirname } = require("path");
 const path = require("path");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 /*sprint 4
 const methodOverride = require('method-override');*/
- 
+
 const router = require("./routes/main");
 const app = express();
 const publicPath = path.resolve(__dirname, "./public");
@@ -17,12 +19,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 */
 
-
-
 app.set("views", [
   path.join(__dirname, "views/products"),
   path.join(__dirname, "views/users"),
-  path.join(__dirname, "views/administradores"),//sprint 4//
+  path.join(__dirname, "views/administradores"), //sprint 4//
   path.join(__dirname, "views/"),
 ]);
 app.listen(process.env.PORT || 3000, () =>
