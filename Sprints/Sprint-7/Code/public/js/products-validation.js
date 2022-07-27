6f48bd2f2ac187217d2000c69d4d0ac2275666ea
Claudio E. Faner <claudiofaner@gmail.com>
2022-07-26 22:42:33 -0300

    update products validation

---
+++ Sprints/Sprint-7/Code/public/js/products-validation.js
@@ -0,0 +1,84 @@
+console.log('form bien llamado')
+window.addEventListener('load',() => {
+
+    // Toma el boton de submit de la vista register, lo pone a 
+    // escuchar para q al hacer click se evite q el formulario se
+    // envie, salvo q la variable errores, q es un array, tenga un
+    // largo menor a 1. Se define variable formulario, para tomarlo y
+    // ejecutar el submit
+    const formBtn = document.querySelector('#btn-form');
+    formBtn.addEventListener('click', (evento)=> {
+    const formulario = document.querySelector('.np-new-product-form')
+    evento.preventDefault();
+    inputs.map(el => {
+    if(el.value.length < 1){
+    createMessage(el);
+    }
+    })
+    if (
+        //(errores < 1) && 
+        (erroresText.length == 0)) {
+    formulario.submit() && console.log('formulario enviado')
+    }
+    });
+    
+    // Definicion de variable errores, q comienza vacia
+    //let errores = 0;
+    let erroresText = [];
+    
+    // Definicion de variable inputs, q toma todos los inputs de cada
+    // campo. Spread operator convierte los nodos en objetos para poder
+    // manipularlos
+    let inputs = document.querySelectorAll('input');
+    inputs = [...inputs];
+    
+    // Map para aplicar a cada campo funciones para colocar o quitar
+    // mensajes de error al falidar; y ademas completar la variable
+    // errores
+    inputs.map(el => {
+     el.onblur = () => {
+     if(el.value.length < 1){
+     createMessage(el);
+     }
+     validateValue(el);
+    // console.log(errores);
+     console.log(erroresText);
+     }
+    })
+    
+    // Funcion q en caso de identificar q el campo esta vacio, inserta el
+    // error como span en el div; y coloca el error en la variable
+    // errores
+    function createMessage(el){
+     let div = el.parentElement;
+     if(div.querySelector('span')){
+     return;
+     }
+     let mensajeError = document.createElement('span');
+     mensajeError.innerText = "El campo está vacío";
+     el.parentElement.append(mensajeError);
+    // errores++;
+     //erroresText.push("El campo <<" + el.name + ">> está vacío") 
+     erroresText.push('error')
+    }
+    
+    // Funcion q en caso de identificar q el campo no esta vacio, borra el
+    // mensaje de error del span en el div; y borra el error en la variable
+    // errores
+    function validateValue(el){
+    let div = el.parentElement;
+    if(el.value.length >= 1){
+     let errorMessage = div.querySelector('span');
+     errorMessage?
+     errorMessage.remove() 
+     : null;
+    // errores--;
+    erroresText?
+    erroresText.pop() 
+    : null
+     }
+    }
+   // errores = Object.keys(errores);
+   // console.log(errores);
+    console.log(erroresText);
+    })
