dcf0c19281ca41fcd6fbcdf5944042f53b9d6082
Marcos Vicente <100540306+marcos-v2@users.noreply.github.com>
2022-07-30 11:20:15 -0300

    Update login-validation.js
    Js de la validación del login no había saltado del sprint 7 al sprint 8

--- Sprints/Sprint-8/Code/public/js/login-validation.js
+++ Sprints/Sprint-8/Code/public/js/login-validation.js
@@ -1,15 +1,24 @@
 window.addEventListener('load',() => {
-    // Evento submit
+ 
+  // Evento submit
 const formBtn = document.querySelector('#btn-form');
-    formBtn.addEventListener('click', (evento)=> {
+  formBtn.addEventListener('click', (evento)=> {
+    const formulario = document.querySelector('.formulario-login');
     evento.preventDefault();     
-    inputs.map(el => {
-      createMessage(el);
-    })
-    if(errores < 1){
-      formBtn.submit();
-    }
 
+    // document.getElementById('email').addEventListener('input', function() {
+    //   campo = e.target;
+      
+  // })
+
+    inputs.map(el => {
+      if(el.value.length < 1){
+        createMessage(el) 
+        }
+      })
+      if(errores < 1){
+    formulario.submit();
+    }
 });
 // Map de inputs con evento onblur
 let errores = 0;
@@ -17,35 +26,48 @@ let errores = 0;
 let inputs = document.querySelectorAll('input');
 inputs = [...inputs];
 inputs.map(el => {
-  el.onblur = () => {
-    if(el.value.length < 1){
-      createMessage(el);
-    }
-    validateValue(el);
-    console.log(errores);
+el.onblur = () => {
+if(el.value.length < 1){
+  createMessage(el);
+}
+validateValue(el);
+// console.log(errores);
 
-  }
-  // if(errores.length >=1){
-    
-  // }
+}
+// if(errores.length >=1){
+campo=inputs[0]
+valido = document.getElementById('emailOK');
+      console.log("válido",valido);
+          
+      emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+      //Se muestra un texto a modo de ejemplo, luego va a ser un icono
+      campo.addEventListener("input", ()=>{if (emailRegex.test(campo.value)) {
+        valido.innerText = "válido";
+errores=errores-2
+} else {
+valido.innerText = "incorrecto";
+errores++  }}); 
+// }
 })
 function createMessage(el){
-  let div = el.parentElement;
-  if(div.querySelector('span')){
-    return;
-  }
-  let mensajeError = document.createElement('span');
-  mensajeError.innerText = "El campo está vacío";
-  el.parentElement.append(mensajeError);
-  errores++; 
+let div = el.parentElement;
+if(div.querySelector('span')){
+return;
+}
+let mensajeError = document.createElement('span');
+mensajeError.innerText = "El campo está vacío";
+el.parentElement.append(mensajeError);
+errores++; 
 }
 function validateValue(el){
-  let div = el.parentElement;
-  if(el.value.length >= 1){
-    let errorMessage = div.querySelector('span');
-    errorMessage.remove();
-    errores--;
-  }
+let div = el.parentElement;
+if(el.value.length >= 1){
+  let errorMessage = div.querySelector('span');
+  errorMessage?
+  errorMessage.remove() 
+  : null; 
+  errores--;
+}
 }
 errores = Object.keys(errores);
 console.log(errores);
