be81c339f7672417aab3fd2473f663b1e06a1cbc
Cristhian Mejias Serrano <cristhianmejias495@gmail.com>
2022-07-31 18:04:05 -0500

    Modificación de controladores

--- Sprints/Sprint-8/Code/controllers/productsControllersDb.js
+++ Sprints/Sprint-8/Code/controllers/productsControllersDb.js
@@ -1,88 +1,81 @@
-
-const db = require('../database-db/models');
+const db = require("../database-db/models");
 const sequelize = db.sequelize;
-const path = require('path');
-
+const path = require("path");
+const Tipo_Articulo = require("../database-db/models/Tipo_Articulo");
 
 const productsControllersDb = {
-    productsList: (req, res) => {
-        db.Product.findAll()
-            .then(productos => {
-                res.render('productList',{productos})//productos está en la vista
-            })
-    },
-    
-    detalleProduct: (req, res) => {
-        db.Product.findByPk(req.params.idProduct)
-            .then(productos => {
-                res.render('productDetail',{productos});
-            });
-    },
+  productsList: (req, res) => {
+    db.Product.findAll().then((productos) => {
+      res.render("productList", { productos }); //productos está en la vista
+    });
+  },
 
-    createProduct: (req, res) => {
-        db.Product.findAll({
-          //
-        })
-            .then(productos => {
-                res.render('newProduct', {productos});
-            });
-    },
-
-        recordProduct: (req, res) => {
-       
-        db.Product.create({ 
-            tipo: req.body.tipo,
-            material: req.body.material,
-            nombre: req.body.nombre,
-            precio: req.body.precio,
-            descripcion: req.body.descripcion,
-            imagen: path.join('/images-multer/', req.file.filename)
-        })
-           
-                   .then(resultado => {
-                        res.redirect('/products')
-                     })
-        
-     }  ,
-
-     editProduct: function(req, res) {
-        db.Product.findByPk(req.params.idProduct)
-        .then((product) => {
-        res.render('editProduct', {product}) } )
-    },
-    
-
-    putProduct: function (req,res) {
-        db.Product.update({ 
-            tipo: req.body.tipo,
-            material: req.body.material,
-            nombre: req.body.nombre,
-            precio: req.body.precio,
-            descripcion: req.body.descripcion,
-            imagen: path.join('/images-multer/', req.file.filename)
-        },
+  detalleProduct: (req, res) => {
+    const { idProduct } = req.params;
+    db.Product.findOne({
+      where: { id_product: idProduct },
+      include: [
         {
-            where : { id_products: req.params.idProduct}  
-           }
-        )
-           
-                   .then(resultado => {
-                        res.redirect("/");
-                     })
-          
-    },
+          association: "categoria",
+        },
+      ],
+    }).then((productos) => {
+      res.render("productDetail", { productos });
+    });
+  },
 
-  
-    
-        deleteProduct: function (req, res) {
-        db.Product.destroy({where: {id_products: req.params.idProduct }})
-        .then(() => {
-                  res.redirect('/products');
-               })
-         }
-    }
+  createProduct: (req, res) => {
+    db.Product.findAll({
+      //
+    }).then((productos) => {
+      res.render("newProduct", { productos });
+    });
+  },
 
+  recordProduct: (req, res) => {
+    db.Product.create({
+      tipo: req.body.tipo,
+      material: req.body.material,
+      nombre: req.body.nombre,
+      precio: req.body.precio,
+      descripcion: req.body.descripcion,
+      imagen: path.join("/images-multer/", req.file.filename),
+    }).then((resultado) => {
+      res.redirect("/products");
+    });
+  },
 
+  editProduct: function (req, res) {
+    db.Product.findByPk(req.params.idProduct).then((product) => {
+      res.render("editProduct", { product });
+    });
+  },
+
+  putProduct: function (req, res) {
+    db.Product.update(
+      {
+        tipo: req.body.tipo,
+        material: req.body.material,
+        nombre: req.body.nombre,
+        precio: req.body.precio,
+        descripcion: req.body.descripcion,
+        imagen: path.join("/images-multer/", req.file.filename),
+      },
+      {
+        where: { id_products: req.params.idProduct },
+      }
+    ).then((resultado) => {
+      res.redirect("/");
+    });
+  },
+
+  deleteProduct: function (req, res) {
+    db.Product.destroy({ where: { id_products: req.params.idProduct } }).then(
+      () => {
+        res.redirect("/products");
+      }
+    );
+  },
+};
 
 module.exports = productsControllersDb;
-
--- Sprints/Sprint-8/Code/database-db/models/Tipo_Articulo.js
+++ Sprints/Sprint-8/Code/database-db/models/Tipo_Articulo.js
@@ -1,30 +1,35 @@
 //modelo de Tipo de Articulo. //
-
-
-
-
-
+const Product = require("./Product");
 
 module.exports = (sequelize, dataTypes) => {
-    let alias = 'Tipo_Articulo';
-    let cols = {
-        id_tipo: {
-            type: dataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        
-        tipo: {
-            type: dataTypes.STRING(400)
-        }
-    
-    }
-    let config = {
-        tableName: 'tipo_articulo',
-        timestamps: false
-    };
-    const Tipo_Articulo = sequelize.define(alias, cols, config)
+  let alias = "Tipo_Articulo";
+  let cols = {
+    id_categoria: {
+      type: dataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
 
-    return Tipo_Articulo
-}
-
+    nombre: {
+      type: dataTypes.STRING(400),
+    },
+  };
+  let config = {
+    tableName: "categoria",
+    timestamps: false,
+  };
+  const Tipo_Articulo = sequelize.define(alias, cols, config);
+  Tipo_Articulo.associate = (models) => {
+    Tipo_Articulo.hasMany(models.Product, {
+      foreignKey: "id_categoria",
+      as: "categoria",
+    });
+  };
+  Product.associate = (models) => {
+    Product.belongsTo(models.Tipo_Articulo, {
+      foreignKey: "id_categoria",
+      as: "categoria",
+    });
+  };
+  return Tipo_Articulo;
+};
