12deea70a6b12c66bbd35c76fabb38d347f0cea4
Claudio E. Faner <claudiofaner@gmail.com>
2022-08-07 16:04:18 -0300

    update carrousel

--- Sprints/Sprint-8/Code/public/js/carrousel.js
+++ Sprints/Sprint-8/Code/public/js/carrousel.js
@@ -17,7 +17,7 @@ punto.forEach( ( cadaPunto , i )=> {
         // Guardar la posición de ese PUNTO
         let posicion  = i
         // Calculando el espacio que debe DESPLAZARSE el GRANDE
-        let operacion = posicion * -50
+        let operacion = posicion * -25
 
         // MOVEMOS el grand
         grande.style.transform = `translateX(${ operacion }%)`
